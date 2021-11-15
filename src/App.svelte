18fb34836ffadebca1ec5e03647d292387da46d2
designingSparks <surfer10@gmail.com>
2021-11-15 23:03:41 +0100

    Snapshot

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
 	import { onMount } from 'svelte';
-	import { Game } from './game'; //imported after the external modules in svelte head
-	import jQuery from '../js/jquery';
+	// import { Game } from './game'; //imported after the external modules in svelte head
+	// import jQuery from '../js/jquery';
+	// import Chessboard from '../js/chessboard-1.0.0'
 	// import Chess from '../js/chess';
-	// import Chessboard from '../js/chessboard';
+	import {Chessboard, jQuery} from './internal';
 
 	export let name; //prop
 	var board;
@@ -12,8 +13,10 @@
 
 	onMount(() => {
 		console.log('Mounted');
-		window.jQuery = jQuery
+		window.jQuery = jQuery;
 		console.log('Created board');
+		board = Chessboard.constructor(el, "start");
+		console.log(Chessboard)
 	});
 
 	function initBoard() {
@@ -53,7 +56,7 @@
 	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script> -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chessboard-js/1.0.0/chessboard-1.0.0.min.css"/>
 	<!-- <script src="chess.js" on:load={initGame}></script> -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/chessboard-js/1.0.0/chessboard-1.0.0.js" on:load={initBoard}> </script>
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/chessboard-js/1.0.0/chessboard-1.0.0.js" on:load={initBoard}> </script> -->
 </svelte:head>
 
 <main>
---
+++ src/internal.js
@@ -0,0 +1,2 @@
+export * from '../js/jquery';
+export * from '../js/chessboard-1.0.0';
